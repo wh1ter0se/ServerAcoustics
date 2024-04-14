35bd39807a550acabc0ccc40ce18980892e21554
wh1ter0se <rckreischer@gmail.com>
2024-04-14 18:49:52 -0400

    Added magnitude spectrograms.

--- plot_spectrograms.py
+++ plot_spectrograms.py
@@ -21,12 +21,24 @@ def plot_spectrogram(mp3_name: str, title: str) -> None:
     wav_path = get_wav_path(mp3_name)
     FS, data = wavfile.read(wav_path)  # read wav file
     plt.figure()
+    plt.title(title)
     plt.specgram(data, Fs=FS, cmap="inferno")  # plot
     plt.xlabel("Time (s)")
     plt.ylabel("Frequency (Hz)")
-    plt.title(title)
     plt.colorbar()
-    plt.savefig(f"figures/{title}.png")
+    plt.savefig(f"figures/specgram_{title}.png")
+
+
+def plot_magspec(mp3_name: str, title: str) -> None:
+    wav_path = get_wav_path(mp3_name)
+    FS, data = wavfile.read(wav_path)  # read wav file
+    plt.figure()
+    plt.grid()
+    plt.magnitude_spectrum(data, Fs=FS)  # plot
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("Level (bits, normalized)")
+    plt.title(title)
+    plt.savefig(f"figures/magspec_{title}.png")
 
 
 plot_spectrogram(mp3_name="serverMicCalibration.mp3", title="Mic Calibration")
@@ -37,3 +49,12 @@ plot_spectrogram(mp3_name="ServerTransient180.mp3", title="Startup (180 deg)")
 plot_spectrogram(mp3_name="ServerTransient240.mp3", title="Startup (240 deg)")
 plot_spectrogram(mp3_name="ServerTransient300.mp3", title="Startup (300 deg)")
 plot_spectrogram(mp3_name="ServerTransientAbove.mp3", title="Startup (above)")
+
+plot_magspec(mp3_name="serverMicCalibration.mp3", title="Mic Calibration")
+plot_magspec(mp3_name="ServerTransient0(gain).mp3", title="Startup (0 deg)")
+plot_magspec(mp3_name="ServerTransient60.mp3", title="Startup (60 deg)")
+plot_magspec(mp3_name="ServerTransient120.mp3", title="Startup (120 deg)")
+plot_magspec(mp3_name="ServerTransient180.mp3", title="Startup (180 deg)")
+plot_magspec(mp3_name="ServerTransient240.mp3", title="Startup (240 deg)")
+plot_magspec(mp3_name="ServerTransient300.mp3", title="Startup (300 deg)")
+plot_magspec(mp3_name="ServerTransientAbove.mp3", title="Startup (above)")
